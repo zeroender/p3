48dca72a7a468f5268bc68bb10dfa4281f4cb93e
Brian Worthington <worthington.brian@gmail.com>
2017-10-10 21:37:41 -0400

    Week 6 Progress log: adding contoller with two dummy methods and two routes

---
+++ app/Http/Controllers/ScheduleController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ScheduleController extends Controller
+{
+  /**
+  * GET /
+  */
+  public function index()
+  {
+    return 'Welcome to the schedule application';
+  }
+
+  public function displaySchedule()
+  {
+    return 'Nothing here yet';
+  }
+
+}
